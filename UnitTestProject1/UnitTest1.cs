868d3fab42ff33c8a094aeec7eceaa810e845024
Anatoly Antonenko <ya.tolik-a@yandex.ru>
2019-04-03 01:00:43 +0300

    unit tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Sum_10and20_30returned()
+        {
+            double x = 10;
+            double y = 20;
+            double expected = 30;
+
+            SomeClass c = new SomeClass();
+            double actual = c.Summ(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        SomeClass someEx = new SomeClass();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+                MessageBox.Show("Данный не введины!");
+            else
+                MessageBox.Show(Convert.ToString(someEx.Summ(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text))));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class SomeClass
+    {
+        public double Summ(double a, double b)
+        {
+            return a + b;
+        }
+
+    }
+}
